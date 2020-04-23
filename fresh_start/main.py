475d1d2091b35a263a4a6a3ad35f2435f15e3426
Daniel Dara <ddara@g.clemson.edu>
2020-04-23 18:37:48 -0400

    Add main file for manual testing.

---
+++ fresh_start/main.py
@@ -0,0 +1,10 @@
+from fresh_start.solve import *
+
+moves = [
+     Move(Boat('X'), Rotation.COUNTER_CLOCKWISE, 180),
+     Move(Wave(3), Cardinal.UP, 1),
+     Move(Boat('Z'), Cardinal.LEFT, 2),
+     Move(Boat('X'), Rotation.COUNTER_CLOCKWISE, 90),
+]
+
+print(find_solution(moves))
