c0df89ac28dc6570bac2976244a2fe73de9b329a
Daniel Dara <ddara@g.clemson.edu>
2020-04-25 20:54:09 -0400

    Continue refactoring design

--- fresh_start/solve.py
+++ fresh_start/solve.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod, ABCMeta
 from copy import deepcopy
 from enum import Enum, EnumMeta
-from typing import List, Dict, Tuple, Set
+from typing import List, Dict, Tuple, Set, Type, Union
 
 puzzle_input = """
 --#-#-###
@@ -16,73 +16,60 @@ puzzle_input = """
 """
 
 
-class Point2D:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+class Position:
+    def __init__(self, row: int, column: int):
+        self.row = row
+        self.column = column
 
 
-class DirectionMeta(ABCMeta, EnumMeta):
+class Direction(Enum):
+    test = '1'
     pass
 
 
-class Direction(ABC):
-    @abstractmethod
-    def shorthand(self, distance: int) -> str:
-        """Returns a short string representation of the current direction and given distance using Solution Notation."""
-        pass
-
-    def validate_distance(self, distance: int) -> None:
-        """Raises a ValueError if the given distance is invalid for the Direction instance."""
-        pass
-
-
-class Cardinal(Direction, Enum, metaclass=DirectionMeta):
+class Cardinal(Direction):
     UP = 'U'
     DOWN = 'D'
     LEFT = 'L'
     RIGHT = 'R'
 
-    def shorthand(self, distance: int) -> str:
-        """Returns a single character representation of the current direction and appends the given distance."""
-        return self.value + str(distance)
 
-
-class Rotation(Direction, Enum, metaclass=DirectionMeta):
-    # There is no corresponding CLOCKWISE value because Solution Notation uses only the counter-clockwise direction.
+class Rotation(Direction):
+    # Rotations are only allowed by the 2-square boat in 2x2 channels. Thus only one rotational direction is needed
+    # since combined with the use of cardinal directions, the same final positions can be achieved.
     COUNTER_CLOCKWISE = 0
-
-    def shorthand(self, distance: int) -> str:
-        """Returns a short string representation of the current direction and given distance."""
-        return '@@' if distance == 180 else '@'
-
-    def validate_distance(self, distance: int) -> None:
-        if distance not in (90, 180):
-            raise ValueError('Rotation distance must be either 90 or 180 degrees.')
 
 
 class Piece(ABC):
-    def __init__(self, id_: str):
+    def __init__(self, id_: Union[str, int]):
         self.id = id_
 
     def validate_direction(self, direction: Direction):
         pass
 
+    @abstractmethod
+    def directions(self) -> List[Direction]:
+        """Return a list of directions that this piece is allowed to move. (independent of board state)"""
+        pass
+
 
 class Boat(Piece):
-    def __init__(self, id_: str, position: Point2D):
+    def __init__(self, id_: str, position: Position):
         super().__init__(id_)
         self.positions = {position}
+
+    def directions(self) -> List[Direction]:
+        # noinspection PyTypeChecker
+        return list(Cardinal) + list(Rotation) if self.id == Puzzle.RED_BOAT_ID else []
 
 
 class Wave(Piece):
     def __init__(self, id_: int, state: str):
-        super().__init__(str(id_))
+        super().__init__(id_)
         self.state = state  # string of visible gaps, blocks, and boats
 
-    def validate_direction(self, direction: Direction):
-        if isinstance(direction, Rotation):
-            raise ValueError('Rotation is not a valid direction for a Wave.')
+    def directions(self) -> List[Direction]:
+        return [Cardinal.LEFT, Cardinal.RIGHT]
 
 
 class Move:
@@ -91,11 +78,8 @@ class Move:
         self.direction = direction
         self.distance = distance
 
-        piece.validate_direction(direction)
-        direction.validate_distance(distance)
-
     def __str__(self) -> str:
-        return self.piece.id + self.direction.shorthand(self.distance)
+        return self.piece.id + self.direction.value
 
     def execute(self) -> None:
         # Waves
@@ -117,9 +101,38 @@ class Solution:
 
 class State:
     """Serializes state information."""
-    def __init__(self, boats: Tuple[Boat], waves: Tuple[Wave]):
+    def __init__(self, boats: Tuple[Boat], waves: Tuple[Wave], is_valid: bool = True):
         self.boats = boats
         self.waves = waves
+        self.is_valid = is_valid
+
+    def pieces(self) -> Tuple[Piece]:
+        # noinspection PyTypeChecker
+        return self.boats + self.waves
+
+    def execute(self, move: Move) -> State:
+        """Executes the given move and returns the resulting state."""
+        new_state = deepcopy(self)
+
+        if isinstance(move.piece, Wave):
+            wave = move.piece
+            pieces = self.get_interlocked_pieces(wave)
+            # TODO reconcile move.piece with piece.execute()...
+            [piece.execute(move) for piece in pieces]  # waves are moved and string state changed, boats are only moved
+            # wave.execute(): remove character in direction of move, add gap at opposite end
+        elif isinstance(move.piece, Boat):
+            boat = move.piece
+
+            for position in boat.positions:
+                self.waves[position.row].clear(position.column)
+
+            boat.execute(move)
+
+            for position in boat.positions:
+                self.waves[position.row].fill(position.column, boat.id)
+
+        # new_state.is_valid == (old gaps, (blocks + boats) == new gaps, (blocks + boats))
+        # return new_state
 
 
 class Puzzle:
@@ -133,22 +146,27 @@ class Puzzle:
         # parse waves (configuration, position)
         self.waves: List[Wave] = []
 
+        self.current_state = None  # Potential alternative to boats/waves members
+
         self.initial_gap_count = 0
         self.initial_block_count = 0
 
     def solve(self) -> Solution:
         """Run a breadth-first search of all possible states until the goal state is reached."""
         moves = []
-        queue: List[Move] = []  # TODO prepopulate a move
+        queue: List[State] = []  # TODO prepopulate a move
         states = Dict[State, Tuple[State, Move]]  # Maps a state to the previous state and move that led to it.
 
         while not self.is_solved() and queue != []:
-            # TODO implement this
-            # pop the next state
-            # for each possible move
-            #   if new state is not visited and is valid
-            #       push state to queue
-            #       states[new_state] = [prev_state, move]
+            self.current_state = queue.pop()
+
+            for piece in self.current_state.pieces():
+                for direction in piece.directions():
+                    new_state = self.current_state.execute(piece, direction)
+
+                    if new_state not in states and new_state.is_valid:
+                        queue.append(new_state)
+                        states[new_state] = (self.current_state, direction)
 
             # TODO functionalize rest of this for the above
 
@@ -196,8 +214,20 @@ class Puzzle:
         #       compare states empty/full space counts
         #       check for broken boats
 
-        # Boats
-        # must not hit boundary
+        # Boat.execute(move)
+        #   row_delta = {up: -1, down: 1}  # rest are 0
+        #   for block in blocks:
+        #       block.row = block.row + row_delta[direction]
+
+        # Boats.is_valid(move)
+        #   all locations in bounds
+        #   no collisions (same gap, block, boat counts)
+
+        #   all locations +-direction in bounds
+        #   no collisions - gap in the space +-direction
+        #       gap_at(row, col) -> ()
+        #       if direction.up -> min(row)
+
 
         # must not hit wave
         # must not hit boat
