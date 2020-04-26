83bc4433a6a697bda4c12f2a15265d8f642374a0
Daniel Dara <ddara@g.clemson.edu>
2020-04-25 23:59:55 -0400

    Properly scope methods and variables. Start interlock implementation.

--- stormyseas/solve.py
+++ stormyseas/solve.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from collections import deque
 from copy import deepcopy
 from enum import Enum
-from typing import List, Dict, Tuple, Union
+from typing import List, Dict, Tuple, Union, Set
 
 puzzle_input = """
 --#-#-###
@@ -92,105 +92,125 @@ class Wave(Piece):
     EMPTY = '-'
     BLOCKED = '#'
 
-    def __init__(self, id_: int, state: str):
+    def __init__(self, id_: int, slider: List[str]):
         super().__init__(id_)
-        self.state = state  # string of visible gaps, blocks, and boats
+        self._slider = slider
 
     def directions(self) -> Tuple[Direction, ...]:
         return Cardinal.LEFT, Cardinal.RIGHT
 
     def move(self, direction: Direction) -> None:
         if direction == Cardinal.LEFT:
-            self.state = self.state[1:] + self.EMPTY
+            self._slider = self._slider[1:] + self.EMPTY
         elif direction == Cardinal.RIGHT:
-            self.state = self.EMPTY + self.state[:-1]
+            self._slider = self.EMPTY + self._slider[:-1]
         else:
             raise ValueError('Invalid move direction for Wave: ' + direction.name)
+
+    def clear(self, column: int) -> None:
+        if 0 <= column < len(self._slider):
+            self._slider[column] = self.EMPTY
+
+    def fill(self, column: int) -> None:
+        if 0 <= column < len(self._slider):
+            self._slider[column] = self.BLOCKED
+
+    def count_gaps(self) -> int:
+        return self._slider.count(self.EMPTY)
+
+    def boat_ids(self) -> Set[str]:
+        return set(filter(lambda char: char != self.EMPTY and char != self.BLOCKED, self._slider))
 
 
 class Move:
     def __init__(self, piece: Piece, direction: Direction, distance: int):
-        self.piece = piece
-        self.direction = direction
-        self.distance = distance
+        self._piece = piece
+        self._direction = direction
+        self._distance = distance
 
     def __str__(self) -> str:
         # noinspection PyTypeChecker
-        return self.piece.id + self.direction.value + str(self.distance)
+        return self._piece.id + self._direction.value + str(self._distance)
 
 
 class Solution:
     def __init__(self, moves: List[Move]):
-        self.moves = moves
+        self._moves = moves
 
     def __str__(self) -> str:
         """Return a string representation of the solution's moves using Solution Notation."""
-        return ', '.join(str(move) for move in self.moves)
+        return ', '.join(str(move) for move in self._moves)
 
 
 class State:
     """Stores state information about the pieces on the board and manages execution of moves."""
 
     def __init__(self, boats: Tuple[Boat], waves: Tuple[Wave], is_valid: bool = True):
-        self.boats = boats
-        self.waves = waves
+        self._boats = boats
+        self._waves = waves
         self.is_valid = is_valid
 
     def is_solved(self) -> bool:
         """Checks if the red boat has reached the finish."""
-        red_boat = next(boat for boat in self.boats if boat.id == Puzzle.RED_BOAT_ID)
+        red_boat = next(boat for boat in self._boats if boat.id == Puzzle.RED_BOAT_ID)
         return red_boat.positions == Puzzle.FINISH_POSITIONS
 
     def pieces(self) -> Tuple[Piece]:
         """Returns a list of all the pieces."""
         # noinspection PyTypeChecker
-        return self.boats + self.waves
+        return self._boats + self._waves
 
     def move(self, piece: Piece, direction: Direction) -> State:
         """Moves the given piece in the given direction and returns a new state."""
         new_state = deepcopy(self)
 
         if isinstance(piece, Wave):
-            for piece in self._find_interlocked_pieces(piece):
-                piece.move(direction)
+            wave: Wave = piece
+
+            for piece in self._find_interlocked_pieces(wave):
+                piece.move(direction)  # --#-- -> ---#-
         elif isinstance(piece, Boat):
-            boat = piece
+            boat: Boat = piece
 
             for position in boat.positions:
-                self.waves[position.row].clear(position.column)
+                self._waves[position.row].clear(position.column)
 
             boat.move(direction)
 
             for position in boat.positions:
-                self.waves[position.row].fill(position.column, boat.id)
+                self._waves[position.row].fill(position.column, boat.id)
 
         self._validate(new_state)
 
         return new_state
 
-    def _find_interlocked_pieces(self, piece: Piece) -> List[Piece]:
-        # TODO implement
-        #   get boats in wave
-        #       get unique boat ids in wave
-        #   get waves for boats
-        #   get new boats
-        #   repeat until no new boats
-        pass
+    def _find_interlocked_pieces(self, first_wave: Wave) -> List[Piece]:
+        # TODO implement (WIP)
+        waves = {first_wave}
+        boat_ids = set()
+
+        while True:
+            new_boat_ids = first_wave.boat_ids() - boat_ids
+
+            new_waves = {}
+            for boat_id in new_boat_ids:
+                new_waves = set(wave for wave in self._waves if boat_id in wave.boat_ids())
+
+            truly_new_waves = new_waves - waves
+
+            # ...
+            break
+
+        return []
 
     def _validate(self, new_state: State) -> None:
-        new_state.is_valid = not new_state._has_same_counts(self) and new_state._has_straight_boats()
-
-    def _has_same_counts(self, other: State) -> bool:
-        return self._gap_count() == other._gap_count() and self._filled_count() == other._filled_count()
+        new_state.is_valid = not new_state._gap_count() == self._gap_count() and new_state._has_straight_boats()
 
     def _gap_count(self) -> int:
-        return sum(wave.state.count('-') for wave in self.waves)
-
-    def _filled_count(self) -> int:
-        return sum([len(wave.state) - wave.state.count('-') for wave in self.waves])
+        return sum(wave.count_gaps() for wave in self._waves)
 
     def _has_straight_boats(self) -> bool:
-        return all(boat.is_straight() for boat in self.boats)
+        return all(boat.is_straight() for boat in self._boats)
 
 
 class Puzzle:
@@ -199,28 +219,28 @@ class Puzzle:
 
     def __init__(self):
         # TODO read input
-        self.initial_state = State(tuple(), tuple())
-        self.current_state = self.initial_state
+        self._initial_state = State(tuple(), tuple())
+        self._current_state = self._initial_state
 
     def solve(self) -> Solution:
         """Finds the shortest set of moves to solve the puzzle using a breadth-first search of all possible states."""
-        queue = deque([self.initial_state])
+        queue = deque([self._initial_state])
 
         # Map of each visited state to its previous state and the move that produced it.
-        states: Dict[State, Union[Tuple[State, Direction], None]] = {self.initial_state: None}
+        states: Dict[State, Union[Tuple[State, Direction], None]] = {self._initial_state: None}
 
-        while not self.current_state.is_solved() and len(queue) > 0:
-            self.current_state = queue.pop(0)
+        while not self._current_state.is_solved() and len(queue) > 0:
+            self._current_state = queue.pop(0)
 
-            for piece in self.current_state.pieces():
+            for piece in self._current_state.pieces():
                 for direction in piece.directions():
-                    new_state = self.current_state.move(piece, direction)
+                    new_state = self._current_state.move(piece, direction)
 
                     if new_state not in states and new_state.is_valid:
                         queue.append(new_state)
-                        states[new_state] = (self.current_state, direction)
+                        states[new_state] = (self._current_state, direction)
 
-        if not self.current_state.is_solved():
+        if not self._current_state.is_solved():
             raise Exception('Puzzle has no solution.')
 
         return self._generate_solution(states)
@@ -230,9 +250,9 @@ class Puzzle:
         """
         moves = []
 
-        while self.current_state != self.initial_state:
-            previous_state, previous_move = states[self.current_state]
+        while self._current_state != self._initial_state:
+            previous_state, previous_move = states[self._current_state]
             moves.insert(0, previous_move)
-            self.current_state = previous_move
+            self._current_state = previous_move
 
         return Solution(moves)
