7fd06ee7253e51683d6ce3ebd2669a79deb21c77
Daniel Dara <ddara@g.clemson.edu>
2020-05-01 01:55:01 -0400

    Fix typo in string formatting.

--- examples/shortest_solution.py
+++ examples/shortest_solution.py
@@ -3,6 +3,6 @@ from stormyseas.solve import Puzzle
 input_ = open('../tests/input/problem_2.txt').read()
 
 solution = Puzzle(input_).shortest_solution()
-print('Solution has %d moves.', solution.length())
+print('Solution has %d moves.' % solution.length())
 print(solution.notation())
 
