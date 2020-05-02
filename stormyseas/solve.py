5cd84357660a42866641a47ab505daedee6556d7
Daniel Dara <ddara@g.clemson.edu>
2020-05-01 21:42:36 -0400

    Simplify state hashing. Move input parsing to inner class. Create and move some constants.

--- stormyseas/solve.py
+++ stormyseas/solve.py
@@ -104,6 +104,8 @@ class Piece(ABC):
 
 
 class Boat(Piece):
+    RED_BOAT_ID = 'X'
+
     @property
     def directions(self) -> Tuple[Direction, ...]:
         # Optimization: The game board is sized such that only 2 length boats will ever have room to rotate.
@@ -112,6 +114,10 @@ class Boat(Piece):
 
 
 class Wave(Piece):
+    LENGTH = 9
+    GAP = '-'
+    BLOCK = '#'
+
     @property
     def directions(self) -> Tuple[Direction, ...]:
         return Cardinal.LEFT, Cardinal.RIGHT
@@ -153,10 +159,11 @@ class State:
     def __init__(self, boats: Tuple[Boat], waves: Tuple[Wave]):
         self._boats = boats
         self._waves = waves
+        self._str_cache = None
 
     def is_solved(self) -> bool:
         """Checks if the red boat has reached the finish."""
-        red_boat = self.find_piece(Puzzle.RED_BOAT_ID)
+        red_boat = self.find_piece(Boat.RED_BOAT_ID)
         return red_boat.positions == Puzzle.FINISH
 
     def pieces(self) -> Iterable[Piece]:
@@ -211,52 +218,43 @@ class State:
 
     def _out_of_bounds(self) -> bool:
         all_rows, all_columns = zip(*[(position.row, position.column) for position in self._all_positions()])
-        return min(all_rows) < 0 or max(all_rows) >= len(self._waves) or min(all_columns) < 0 or max(all_columns) >= 9
+        return (
+            min(all_rows) < 0 or max(all_rows) >= len(self._waves)
+            or min(all_columns) < 0 or max(all_columns) >= Wave.LENGTH
+        )
 
     def _all_positions(self) -> Iterable[Position]:
         return chain.from_iterable(piece.positions for piece in self.pieces())
 
+    def __eq__(self, other) -> bool:
+        return self.__str__() == other.__str__()
+
+    def __hash__(self) -> int:
+        return hash(self.__str__())
+
     def __str__(self) -> str:
-        board = [['-'] * 9 for _ in range(8)]
+        # TODO Cleanup caching, perhaps make State immutable and put move/push in a new or existing class.
+        if self._str_cache is None:
+            board = [[Wave.GAP] * Wave.LENGTH for _ in range(len(self._waves))]
 
-        for wave in self._waves:
-            for position in wave.positions:
-                board[position.row][position.column] = '#'
+            for wave in self._waves:
+                for position in wave.positions:
+                    board[position.row][position.column] = Wave.BLOCK
 
-        for boat in self._boats:
-            for position in boat.positions:
-                board[position.row][position.column] = boat.id
+            for boat in self._boats:
+                for position in boat.positions:
+                    board[position.row][position.column] = boat.id
 
-        return '\n'.join(''.join(row) for row in board)
+            self._str_cache = '\n'.join(''.join(row) for row in board)
 
-
-Size = namedtuple('Size', 'rows columns')
+        return self._str_cache
 
 
 class Puzzle:
-    SIZE = Size(8, 9)
-    RED_BOAT_ID = 'X'
     FINISH = {Position(6, 5), Position(7, 5)}
 
     def __init__(self, input_: str):
-        boat_positions: Dict[str, Set[Position]] = defaultdict(lambda: set())
-        waves = []
-
-        for row, line in enumerate(input_.strip().split('\n')):
-            wave_positions = set()
-
-            for column, character in enumerate(line.strip()):
-                if character == '#':
-                    wave_positions.add(Position(row, column))
-                elif character != '-':
-                    boat_positions[character].add(Position(row, column))
-
-            # Constraint: Rows are 1-based in solution notation.
-            waves.append(Wave(str(row + 1), wave_positions))
-
-        boats = [Boat(id_, positions) for id_, positions in boat_positions.items()]
-
-        self._initial_state = State(tuple(boats), tuple(waves))
+        self._initial_state = self._Input(input_).parse_state()
         self._current_state = self._initial_state
 
     def shortest_solution(self) -> Solution:
@@ -274,7 +272,7 @@ class Puzzle:
         queue = deque([(self._initial_state, 0)])
 
         # Map of each visited state to its previous state and the move that produced it.
-        states: Dict[str, Union[Tuple[State, Move, int], None]] = {str(self._initial_state): None}
+        states: Dict[State, Union[Tuple[State, Move, int], None]] = {self._initial_state: None}
 
         previous_steps = 0
         previous_states_length = len(states)
@@ -293,9 +291,9 @@ class Puzzle:
                 for direction in piece.directions:
                     new_state = self._current_state.move(piece, direction)
 
-                    if new_state.is_valid() and str(new_state) not in states:
+                    if new_state.is_valid() and new_state not in states:
                         queue.append((new_state, steps + 1))
-                        states[str(new_state)] = (self._current_state, Move(piece, direction), steps + 1)
+                        states[new_state] = (self._current_state, Move(piece, direction), steps + 1)
 
             if steps != previous_steps:
                 seconds = time() - start_time
@@ -339,8 +337,8 @@ class Puzzle:
 
         current_state = self._current_state
 
-        while str(current_state) != str(self._initial_state):
-            previous_state, previous_move, steps = states[str(current_state)]
+        while current_state != self._initial_state:
+            previous_state, previous_move, steps = states[current_state]
             moves.insert(0, previous_move)
             current_state = previous_state
 
@@ -349,3 +347,27 @@ class Puzzle:
     class _SearchType(Enum):
         SHORTEST = 0
         ALL = 1
+
+    class _Input:
+        def __init__(self, input_: str):
+            self.input = input_
+
+        def parse_state(self) -> State:
+            boat_positions: Dict[str, Set[Position]] = defaultdict(lambda: set())
+            waves = []
+
+            for row, line in enumerate(self.input.strip().split('\n')):
+                wave_positions = set()
+
+                for column, character in enumerate(line.strip()):
+                    if character == Wave.BLOCK:
+                        wave_positions.add(Position(row, column))
+                    elif character != Wave.GAP:
+                        boat_positions[character].add(Position(row, column))
+
+                # Constraint: Rows are 1-based in solution notation.
+                waves.append(Wave(str(row + 1), wave_positions))
+
+            boats = tuple(Boat(id_, positions) for id_, positions in boat_positions.items())
+
+            return State(boats, tuple(waves))
